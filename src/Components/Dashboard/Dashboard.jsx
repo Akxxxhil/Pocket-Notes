4a32db47c27e5301eb3238f1c67e6410321b49db
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-05-05 01:00:20 +0530

    2 nd page started

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,20 +1,35 @@
 import React, { useState } from "react";
 import "./Dashboard.css";
+import Notes from "../Notes/Notes";
 
 function Dashboard() {
   const [popup, setPopup] = useState(false);
-  const [title, setTilte] = useState("")
+  const[pageActive,setPageActive]=useState(false)
+  const [title, setTilte] = useState("");
+  const [colorpicker, setColorpicker] = useState();
   const [notesgroup, setNotesgroup] = useState([]);
+
+  const isCreateEnabled = title.trim() !== "" && colorpicker !== "";
   function groupcreateHandler() {
-    setNotesgroup((prevState) => [...prevState, { title: title }])
-    setTilte("")
-    setPopup(false)
+    setNotesgroup((prevState) => [
+      ...prevState,
+      { title: title, colorpicker: colorpicker },
+    ]);
+    //setColorpicker("#B38BFA"); // Reseting color to default after group creation
+    setTilte(""); //Reseting my input value
+    setPopup(false);
   }
   function createHandler() {
     setPopup(!popup);
   }
   function titleHandler(e) {
-    setTilte(e.target.value)
+    setTilte(e.target.value);
+  }
+  function colorchooser(color) {
+    setColorpicker(color);
+  }
+  function pageHandler(){
+setPageActive(true)
   }
   return (
     <div>
@@ -22,15 +37,37 @@ function Dashboard() {
         <div className="notes-group-dash">
           <h1 className="roboto-bold">Pocket Notes</h1>
           <div>
-            {notesgroup.map((eachGroup,index) => {
-              return <div className="eachgroup" key={index}>{eachGroup.title}</div>
+            {notesgroup.map((eachGroup, index) => {
+              return (
+                <div
+                  className="eachgroup"
+                  key={index}
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <span
+                    style={{
+                      backgroundColor: eachGroup.colorpicker,
+                      borderRadius: "50%",
+                      padding: "10px",
+                      cursor: "pointer",
+                      display: "inline-block",
+                      width: "30px",
+                      height: "30px",
+                    }}
+                  ></span>
+                  <span onClick={pageHandler} style={{ marginTop: "8px", marginLeft: "8px",cursor:"pointer" }}>
+                    {eachGroup.title}
+                  </span>
+                </div>
+              );
             })}
           </div>
           <button onClick={createHandler} className="create-btn">
             Create
           </button>
         </div>
-        <div className="notes-dash">
+        {pageActive ? (<Notes/>):(
+          <div className="notes-dash">
           <img
             style={{
               marginLeft: "auto",
@@ -63,25 +100,52 @@ function Dashboard() {
             end-to-end encrypted
           </p>
         </div>
+        )}
       </div>
       {popup && (
         <div className="popup-overlay">
           <div className="popup">
             <p>Create New group</p>
             <label htmlFor="">Group Name </label>
-            <input value={title} onChange={titleHandler} type="text" id="title" placeholder="Enter note title" />
+            <input
+              value={title}
+              onChange={titleHandler}
+              type="text"
+              id="title"
+              placeholder="Enter note title"
+            />
             <br />
             <br />
             <label htmlFor="">Choose colour </label>
-            <span className="color1-option color-chooser"></span>
-            <span className="color2-option color-chooser"></span>
-            <span className="color3-option color-chooser"></span>
-            <span className="color4-option color-chooser"></span>
-            <span className="color5-option color-chooser"></span>
-            <span className="color6-option color-chooser"></span>
+            <span
+              onClick={() => colorchooser("#B38BFA")}
+              className="color1-option color-chooser"
+            ></span>
+            <span
+              onClick={() => colorchooser("#FF79F2")}
+              className="color2-option color-chooser"
+            ></span>
+            <span
+              onClick={() => colorchooser("#43E6FC")}
+              className="color3-option color-chooser"
+            ></span>
+            <span
+              onClick={() => colorchooser("#F19576")}
+              className="color4-option color-chooser"
+            ></span>
+            <span
+              onClick={() => colorchooser("#0047FF")}
+              className="color5-option color-chooser"
+            ></span>
+            <span
+              onClick={() => colorchooser("#6691FF")}
+              className="color6-option color-chooser"
+            ></span>
             <br />
             <br />
-            <button onClick={groupcreateHandler} type="submit">Create</button>
+            <button onClick={groupcreateHandler}disabled={!isCreateEnabled} type="submit">
+              Create
+            </button>
           </div>
         </div>
       )}
---
+++ src/Components/Notes/Notes.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import "./Notes.css"
+
+function Notes() {
+    const[allnotes,setAllNotes]=useState([])
+    const [text, setText] = useState("")
+    function addedNote(e) {
+        setText(e.target.value)
+    }
+    function newNote(){
+        setAllNotes((prevState)=>[...allnotes,text])
+        setText("")
+    }
+    
+
+    return (
+        <div>
+           <div >
+           <div>
+                {allnotes.map((singleNote)=>{
+                    return <div className='singleNote'>{singleNote}</div>
+                })}
+            </div>
+            <div style={{ position: "absolute", bottom: "0", width: "75%" }}>
+                <div style={{ border: "2px solid white", position: "relative" }}>
+                    <textarea
+                    value={text}
+                        onChange={addedNote}
+                        name=""
+                        id=""
+                        rows="5"
+                        cols="120"
+                        style={{ width: "100%" }}
+                        placeholder='Enter your text here...........'
+                    ></textarea>
+                    <button disabled={text.length === 0}
+                    onClick={newNote}
+                        style={{
+                            position: "absolute",
+                            right: "10px",
+                            bottom: "10px",
+                            zIndex: "1", // To make sure the button stays on top of the textarea
+                        }}
+                    >
+                        Your Button
+                    </button>
+                </div>
+            </div>
+           </div>
+        </div>
+    );
+}
+
+export default Notes;
