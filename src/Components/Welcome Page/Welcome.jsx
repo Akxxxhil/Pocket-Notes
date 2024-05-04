84d2ad5e36e4cbc5e8de41ac7d71a2ea172d7e4d
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-05-04 09:10:10 +0530

    1 st page functionality done

--- src/Components/Welcome Page/Welcome.jsx
+++ src/Components/Welcome Page/Welcome.jsx
@@ -1,55 +1,83 @@
-import React, { useState } from 'react'
-import "./Welcome.css"
+import React, { useState } from 'react';
+import "./Welcome.css";
 
 function Welcome() {
-    const [showinput, setShowInput] = useState(false)
+    const [showInput, setShowInput] = useState(false);
+    const [titleNote, setTitleNote] = useState("");
+    const [allGroup, setAllGroup] = useState([]);
+    const [selectedColor, setSelectedColor] = useState("green"); // Default color
 
-    const [titlenote, setTitleNote] = useState("")
-    const [allgroup, setAllGroup] = useState([])
-
-
-
-    function clickhandler() {
-        setShowInput(!showinput);
-
+    function clickHandler() {
+        setShowInput(!showInput);
     }
-    function titleHnadler(e) {
-        setTitleNote(e.target.value)
+
+    function titleHandler(e) {
+        setTitleNote(e.target.value);
     }
-    function adddata() {
-        setAllGroup(prevState => [...prevState, titlenote])
-        setTitleNote("")
+
+    function addData() {
+        // Splitting the title into words
+        const titleWords = titleNote.split(" ");
+        // Getting the initials
+        let initials = "";
+        if (titleWords.length > 0) {
+            // If the first word is longer than one letter, take the first two letters
+            if (titleWords[0].length > 1) {
+                initials = titleWords[0].slice(0, 2).toUpperCase();
+            } else {
+                // Otherwise, take the first letter of each word
+                initials = titleWords.map(word => word[0].toUpperCase()).join("");
+            }
+        }
+
+        // Creating the note object
+        const newNote = {
+            title: titleNote,
+            color: selectedColor,
+            initials: initials
+        };
+
+        // Updating the state
+        setAllGroup(prevState => [...prevState, newNote]);
+        setTitleNote("");
     }
+
+    function changeColor(color) {
+        setSelectedColor(color);
+    }
+
     return (
         <>
             <div className='welcome-page'>
                 <h1 className='heading'>Pocket Notes</h1>
                 <div>
-                    
-                    {allgroup.map((item, index) => (
-                        
-                    <div style={{border:"5px solid red",margin:"5px",padding:"8px",paddingRight:"10px"}} key={index}>
-                        <span style={{border:"5px solid green",margin:"3px",padding:"5px",borderRadius:"50%"}} ></span>{item}</div>
-                ))}</div>
-                <button onClick={clickhandler} className='add-btn'>Add</button>
+                    {allGroup.map((item, index) => (
+                        <div
+                            key={index}
+                            style={{ border: `5px solid ${item.color}`, margin: "5px", padding: "8px", paddingRight: "10px" }}
+                        >
+                            <span style={{ backgroundColor: item.color, margin: "3px", padding: "5px", borderRadius: "50%" }} >{item.initials}</span>{item.title}
+                        </div>
+                    ))}
+                </div>
+                <button onClick={clickHandler} className='add-btn'>Add</button>
             </div>
-            <div className='title-input' style={{ display: showinput ? "block" : "none" }}>
-
-                {showinput && (
+            <div className='title-input' style={{ display: showInput ? "block" : "none" }}>
+                {showInput && (
                     <div>
                         <label htmlFor="">Enter the title:  </label>
-                        <input onChange={titleHnadler} value={titlenote} type="text" id='title' placeholder="Enter note title" /><br /><br />
+                        <input onChange={titleHandler} value={titleNote} type="text" id='title' placeholder="Enter note title" /><br /><br />
                         <label htmlFor="">Enter the Color:  </label>
-                        <span style={{ backgroundColor: "red" }}>Red</span>
-                        <span style={{ backgroundColor: "green" }}>Green</span>
-                        <span style={{ backgroundColor: "blue" }}>Blue</span>
-                        <span style={{ backgroundColor: "brown" }}>Brown</span><br /><br />
-                        <button onClick={adddata} type="submit">submit</button>
+                        <span onClick={() => changeColor("red")} style={{ backgroundColor: "red", borderRadius: "50%", padding: "10px", cursor: "pointer", display: "inline-block", width: "30px", height: "30px" }}></span>
+                        <span onClick={() => changeColor("green")} style={{ backgroundColor: "green", borderRadius: "50%", padding: "10px", cursor: "pointer", display: "inline-block", width: "30px", height: "30px" }}></span>
+                        <span onClick={() => changeColor("blue")} style={{ backgroundColor: "blue", borderRadius: "50%", padding: "10px", cursor: "pointer", display: "inline-block", width: "30px", height: "30px" }}></span>
+                        <span onClick={() => changeColor("brown")} style={{ backgroundColor: "brown", borderRadius: "50%", padding: "10px", cursor: "pointer", display: "inline-block", width: "30px", height: "30px" }}></span><br /><br />
+                        <button onClick={addData} type="submit">Create</button>
                     </div>
                 )}
             </div>
         </>
-    )
+    );
 }
 
-export default Welcome
+export default Welcome;
